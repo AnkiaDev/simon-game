4a2a279d94262123f6143942bd99ddcc6179a95b
Ankia <AnkiaDev@gmail.com>
2020-07-19 16:04:24 +0200

    write JS program, optimise and add commentary

--- index.js
+++ index.js
@@ -1,38 +1,97 @@
+let letsPlay = 0;
 const simonSequence = [];
 const playerSequence = [];
-const greenSound = new Audio("./sounds/green.mp3");
-const redSound = new Audio("./sounds/red.mp3");
-const yellowSound = new Audio("./sounds/yellow.mp3");
-const blueSound = new Audio("./sounds/blue.mp3");
-const wrongSound = new Audio("./sounds/wrong.mp3");
+
+const opacityAnim = (color) => {
+  $(`.${color}`)
+    .animate({ opacity: "0.5" }, "fast")
+    .animate({ opacity: "1" }, "fast");
+};
+
+const playSound = (color) => {
+  new Audio(`./sounds/${color}.mp3`).play();
+};
 
 const simonPlay = () => {
+  //empty playerSequence array
+  playerSequence.splice(0, playerSequence.length);
+  //Display the sequence level
+  $(".title-text").text(`Level ${simonSequence.length + 1}`);
+  //In one second...
   setTimeout(() => {
+    //...choose a number between 0 and 3...
     const randomNumb = Math.floor(Math.random() * 4);
+    //...depending on the number...
     switch (randomNumb) {
+      //...choose a color square...
       case 0:
+        //...add the color of the square to the simonSequence array...
         simonSequence.push("green");
-        greenSound.play();
+        //...make an animation on the square for the player...
+        opacityAnim("green");
+        //...and play the sound of the square.
+        playSound("green");
         break;
       case 1:
         simonSequence.push("red");
-        redSound.play();
+        opacityAnim("red");
+        playSound("red");
         break;
       case 2:
         simonSequence.push("yellow");
-        yellowSound.play();
+        opacityAnim("yellow");
+        playSound("yellow");
         break;
       case 3:
         simonSequence.push("blue");
-        blueSound.play();
+        opacityAnim("blue");
+        playSound("blue");
         break;
       default:
         console.log("Error");
     }
-    console.log("in simonPlay func", simonSequence);
   }, 1000);
 };
 
+// The player press a key, the game begin
 $(document).keypress(() => {
-  simonPlay();
+  if (letsPlay === 0) {
+    // if the game has begun don't call simonPlay() again when keypress
+    letsPlay = 1;
+    simonPlay();
+  }
+});
+
+// When player click a square...
+$(".square").click(function () {
+  const colorClicked = $(this)[0].classList[1];
+  //...add the color of the square clicked to the playerSequence array...
+  playerSequence.push(colorClicked);
+  //...make an animation on the square cliked...
+  opacityAnim(colorClicked);
+  //...play the sound...
+  new Audio(`./sounds/${colorClicked}.mp3`).play();
+  //...compare each clicked square color to the simonSequence with a loop...
+  for (let i = 0; i < playerSequence.length; i++) {
+    //...if the color is not the same...
+    if (playerSequence[i] !== simonSequence[i]) {
+      //...play the wrong sound...
+      playSound("wrong");
+      //... display red color to the background...
+      $("body").css("background-color", "red");
+      setTimeout(() => {
+        $("body").css("background-color", "#011f3f");
+      }, 200);
+      //...display the game over message...
+      $(".title-text").text("Game Over, Press Any Key to Restart");
+      simonSequence.splice(0, simonSequence.length);
+      //...make the keypress work again for begin a new game.
+      letsPlay = 0;
+      return;
+    }
+  }
+  //If the colors are the same, the game add new color to the simonSequence.
+  if (playerSequence.length === simonSequence.length) {
+    simonPlay();
+  }
 });
